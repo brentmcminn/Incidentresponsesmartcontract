4a0461d660f4022c63c9fa7affa035fd8cec5277
Michael Ferber <mferber8@gmail.com>
2020-01-18 13:14:57 -0600

    event now a dictionary

--- mike/monitor_contract.py
+++ mike/monitor_contract.py
@@ -16,12 +16,33 @@ mycontract = web3.eth.contract(address='0x091FDeb7990D3E00d13c31b81841d56b33164A
 
 def my_callback(event):
     print (f'{event} fired from contract')
+    print(type(event))
+    
+def toDict(dictToParse):
+    # convert any 'AttributeDict' type found to 'dict'
+    parsedDict = dict(dictToParse)
+    for key, val in parsedDict.items():
+        # check for nested dict structures to iterate through
+        if  'dict' in str(type(val)).lower():
+            parsedDict[key] = toDict(val)
+        # convert 'HexBytes' type to 'str'
+        elif 'HexBytes' in str(type(val)):
+            parsedDict[key] = val.hex()
+    return parsedDict
 
 myfilter = mycontract.events.currentResponderState.createFilter(fromBlock=16147303)
 
 while True:
    for event in myfilter.get_new_entries():
-        my_callback(event)
+        #my_callback(event)
+        event_dict = toDict(event)
+        event_dict = json.dumps(event_dict,indent=4)
+        print(event_dict)
+        print(event_dict[0])
+        print(event_dict[1])
+        #print (event_dict["args"])
+        #print (event_dict["args"][0]['sensorName'])
+
    time.sleep(2)
 
 
